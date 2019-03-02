b9b9d615404ee15e40c5e6c79955c42b3a555068
victor.ganjos <victor.ganjos@CAS555509W1010.senacsp.edu.br>
2019-03-01 21:59:21 -0300

    versão inicial

--- geren-prod/src/main/java/br/senac/tads/pi/geren/prod/MainProd.java
+++ geren-prod/src/main/java/br/senac/tads/pi/geren/prod/MainProd.java
@@ -3,5 +3,7 @@ package br.senac.tads.pi.geren.prod;
 public class MainProd {
     public static void main(String[] args) {
         System.out.println("Teste Maven");
+        int i=0;
+        System.out.println("Valor de i++"+ i++);
     }
 }
